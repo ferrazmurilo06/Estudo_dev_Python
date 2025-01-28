b6a8cc05adbdbba96998826be12989346a8d1835
Murilo A. Ferraz <muriloalbuquerqueferraz@gmail.com>
2025-01-28 16:52:16 -0300

    second commit because the first goes wrong

---
+++ aula08.py
@@ -0,0 +1,13 @@
+nome = 'Murilo'
+sobrenome = 'Albuquerque Ferraz'
+idade = 20
+ano_nascimento = 2004
+maior_de_idade = idade >= 18
+altura = 1.78
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de Nascimento: ', ano_nascimento)
+print('É maior de idade?: ', maior_de_idade)
+print('Altura: ', altura)
---
+++ aula12.py
@@ -0,0 +1,8 @@
+nome = "Murilo"
+altura = 1.78
+peso = 112
+imc = peso/(altura**2)
+
+print(nome, "tem", altura, "de altura.")
+print("Pesa", peso, "quilos e seu IMC é:")
+print(imc)
